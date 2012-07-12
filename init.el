e7cbfecb8f2a8786d5afa147affbb056b3a9f9b0
Rick Dillon <rpdillon@verticalbrands.com>
2012-07-12 11:33:57 -0700

    Adding Helm, turning on line highlighting, cleaning up init.el.

--- init.el
+++ init.el
@@ -15,20 +15,3 @@
 
 ; Load all literate org-mode files in this directory (any org-mode files residing there)
 (mapc #'org-babel-load-file (directory-files dotfiles-dir t "emacs.org$"))
-(custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(epa-popup-info-window nil)
- '(erc-log-insert-log-on-open nil)
- '(erc-modules (quote (autojoin button completion fill irccontrols list log match menu move-to-prompt netsplit networks noncommands readonly ring stamp track)))
- '(htmlize-output-type (quote font))
- '(org-confirm-elisp-link-function nil)
- '(todotxt-file "/home/rpdillon/Dropbox/memex/todo.txt" nil (todotxt)))
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- )
